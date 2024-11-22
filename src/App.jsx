b94bf94c12eab7e580d5caa30e1540febab03cdc
SanketLV <sanketlakhani80@gmail.com>
2024-11-22 22:54:35 +0530

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const app = ( ) => {
+    return <main>
+        <h1 className="text-5xl text-violet-300">Welcome to Awwwards</h1>
+    </main>
+}
+
+export default app
