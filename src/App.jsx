63e769f00830f0d7190a5b839c1a811f06a38fdf
SanketLV <sanketlakhani80@gmail.com>
2024-11-24 10:28:11 +0530

    feature section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Feature from "./components/Feature";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ const app = () => {
       <Navbar />
       <Hero />
       <About />
+      <Feature />
     </main>
   );
 };
